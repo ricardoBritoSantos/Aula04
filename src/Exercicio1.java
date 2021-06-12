0648802925b454fdcb53b77a42c69fe4c2031553
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-12 15:35:38 -0300

    exercicio 1 ok

---
+++ src/Exercicio1.java
@@ -0,0 +1,16 @@
+//01 - Crie um programa que seja capaz de exibir todos os números inteiros entre 0 e 1000.
+//(Se você conseguir, modifique o programa para que mostre apenas os números pares)
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		var i =0;
+		while(i < 1001) {
+			System.out.println(i);
+			i +=2;
+		}
+	}
+
+}
