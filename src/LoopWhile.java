23a8df7f5ff179251a4de60da29b6e2f8a651585
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-12 15:22:01 -0300

    Implementado o loop While

--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class LoopWhile {
 
@@ -5,6 +6,19 @@ public class LoopWhile {
 		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
 		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
 
+		var scanner = new Scanner(System.in);
+		int idade = 0;
+		System.out.println("Digite a quantidade de alunos");
+		var alunos = scanner.nextInt();
+		var conta = alunos;
+		while(alunos > 0) {
+			System.out.println("Digite a idade do primero aluno");
+			idade += scanner.nextInt();
+			alunos--;
+		}
+		
+		System.out.println("A média de idade dos alunos é de:" + (idade/ conta));
+		scanner.close();
 	}
-
+	
 }
